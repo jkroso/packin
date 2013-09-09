21dc23fc639d142a063b1a1078b2c556f9ff235a
Jake Rosoman <jkroso@gmail.com>
2013-09-09 14:25:45 +1200

    remove json parser
    not sure it really has that much win

--- src/latest-npm.js
+++ src/latest-npm.js
@@ -1,48 +1,30 @@
 
-var parseJSON = require('JSONStream').parse
-var concat = require('concat-stream')
+var get = require('solicit/node').get
 var defer = require('result/defer')
-var get = require('./http-get')
 var semver = require('semver')
-
-exports.tag = latestTag
 
 /**
  * get the latest tag from npm.org that matches `vSpec`
- * 
+ *
  * @param {String} name
  * @param {String} vSpec
  * @return {DeferredResult} string
  */
 
-function latestTag(name, vSpec){ return defer(function(write, fail){
-	get('http://registry.npmjs.org/'+name).then(function(res){
-		res.pipe(parseJSON(['versions', match(vSpec)]))
-			.pipe(concat(function(e, versions){
-				if (e) return fail(e)
-				if (!versions || !versions.length) {
-					return fail(new Error(name+'@'+vSpec+' not in npm'))
-				}
-				var latest = versions
-					.map(getVersion)
-					.sort(semver.rcompare)[0]
-				write(latest)
-			}))
+exports.tag = function(name, vSpec){
+	return get('http://registry.npmjs.org/' + name).then(function(json){
+		var tags = Object.keys(json.versions).filter(function(tag){
+			return semver.valid(tag) && semver.satisfies(tag, vSpec)
+		})
+		if (!tags.length) {
+			throw new Error(name + '@' + vSpec + ' not in npm')
+		}
+		return tags.sort(semver.rcompare)[0]
 	})
-})}
+}
 
-exports.url = function(name, vSpec){
-	return latestTag(name, vSpec).then(function(tag){
+exports.url = function(name, vSpec){ return defer(function(){
+	return exports.tag(name, vSpec).then(function(tag){
 		return 'http://registry.npmjs.org/'+name+'/-/'+name+'-'+tag+'.tgz'
 	})
-}
-
-function getVersion(obj){
-	return obj.version
-}
-
-function match(spec){
-	return function(version){
-		return semver.valid(version) && semver.satisfies(version, spec)
-	}
-}
+})}
