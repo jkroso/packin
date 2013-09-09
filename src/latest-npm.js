41d15f214bc6db2cde7638bfdb03cab8363babc3
Jake Rosoman <jkroso@gmail.com>
2013-09-09 18:17:22 +1200

    tidy

--- src/latest-npm.js
+++ src/latest-npm.js
@@ -2,27 +2,46 @@
 var get = require('solicit/node').get
 
 /**
- * get the latest tag from npm.org that matches `vSpec`
+ * get `name`'s latest tag which matches `spec`
  *
  * @param {String} name
  * @param {String} spec
  * @return {Result} string
+ * @api public
  */
 
 exports.tag = function(name, spec){
-	return get('http://registry.npmjs.org/' + name + '/' + spec)
-		.type('json')
-		.then(getVersion, function(){
-			throw new Error(name + '@' + spec + ' not in npm')
-		})
+	return latest(name, spec).then(getVersion, function(res){
+		throw new Error(res.message + ' ' + name)
+	})
 }
 
+/**
+ * get the latest tarball url for name@spec
+ *
+ * @param {String} name
+ * @param {String} spec
+ * @return {Result} string
+ * @api public
+ */
+
 exports.url = function(name, spec){
+	return latest(name, spec).then(getDist, function(res){
+		throw new Error(res.message + ' ' + name)
+	})
+}
+
+/**
+ * get name@spec's latest package.json from npm.org
+ *
+ * @param {String} name
+ * @param {String} spec
+ * @return {Result}
+ * @api private
+ */
+
+function latest(name, spec){
 	return get('http://registry.npmjs.org/' + name + '/' + spec)
-		.type('json')
-		.then(getDist, function(){
-			throw new Error(name + '@' + spec + ' not in npm')
-		})
 }
 
 function getDist(json){
